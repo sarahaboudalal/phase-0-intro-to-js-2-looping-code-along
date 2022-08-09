8114b7c43747e1949676e4bab7fd97d3078f11b1
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-08-09 22:05:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(theirArray) {
+    let myArra = [];
+    let counter = 0;
+    theirArray.forEach(element => {
+        myArra[counter] = `Thank you, ${element}, for the wonderful surprise gift!`
+        counter++;
+    });
+    return myArra;
+}
+
+function countDown(num) {
+   for (let i = num; i >= 0; i--)
+   console.log(i)
+}
